5662583f66d5b13dfd982d8b32d35dd27fb5b473
stefanradulovicuns <stefanradulovic@uns.ac.com>
2021-06-08 18:13:00 +0200

    Endpoints updated, redirects file created

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,13 @@
+/*
 export const STATUS_URL = 'http://localhost:8083/status';
 export const FAKULTET_URL = 'http://localhost:8083/fakultet';
 export const DEPARTMAN_URL = 'http://localhost:8083/departman';
 export const STUDENT_URL = 'http://localhost:8083/student';
 export const STUDENTI_ZA_DEPARTMAN_URL = 'http://localhost:8083/studentiDepartman';
+*/
+
+export const STATUS_URL = 'https://rva-backend-it6g2018.herokuapp.com/status';
+export const FAKULTET_URL = 'https://rva-backend-it6g2018.herokuapp.com/fakultet';
+export const DEPARTMAN_URL = 'https://rva-backend-it6g2018.herokuapp.com/departman';
+export const STUDENT_URL = 'https://rva-backend-it6g2018.herokuapp.com/student';
+export const STUDENTI_ZA_DEPARTMAN_URL = 'https://rva-backend-it6g2018.herokuapp.com/studentiDepartman';
